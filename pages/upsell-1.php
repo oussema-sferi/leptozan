534c6a6624c7c8d62c9e5e0eb2cd1d0246aa87cf
oussema-sferi <oussema.sferi@gmail.com>
2025-01-25 18:26:53 +0100

    upsell - upgrade button width fixed 2

--- pages/upsell-1.php
+++ pages/upsell-1.php
@@ -74,6 +74,19 @@
 							<img src="/images/play-icon.svg" alt="play-icon" class="img-fluid">
 						</figure>
 					</a>
+                    <!-- Vidalytics Embed -->
+                    <!--<div id="vidalytics_embed_oo86lpbKzHm8_r2O" style="width: 100%; position:relative; padding-top: 56.25%;"></div>
+                    <script type="text/javascript">
+                        (function (v, i, d, a, l, y, t, c, s) {
+                            y='_'+d.toLowerCase();c=d+'L';if(!v[d]){v[d]={};}if(!v[c]){v[c]={};}if(!v[y]){v[y]={};}var vl='Loader',vli=v[y][vl],vsl=v[c][vl + 'Script'],vlf=v[c][vl + 'Loaded'],ve='Embed';
+                            if (!vsl){vsl=function(u,cb){
+                                if(t){cb();return;}s=i.createElement("script");s.type="text/javascript";s.async=1;s.src=u;
+                                if(s.readyState){s.onreadystatechange=function(){if(s.readyState==="loaded"||s.readyState=="complete"){s.onreadystatechange=null;vlf=1;cb();}};}else{s.onload=function(){vlf=1;cb();};}
+                                i.getElementsByTagName("head")[0].appendChild(s);
+                            };}
+                            vsl(l+'loader.min.js',function(){if(!vli){var vlc=v[c][vl];vli=new vlc();}vli.loadScript(l+'player.min.js',function(){var vec=v[d][ve];t=new vec();t.run(a);});});
+                        })(window, document, 'Vidalytics', 'vidalytics_embed_oo86lpbKzHm8_r2O', 'https://fast.vidalytics.com/embeds/oCMpPuWk/oo86lpbKzHm8_r2O/');
+                    </script>-->
 				</div>
 			</div>
 		</div>
@@ -145,7 +158,7 @@
 									</ul>
 								</div>
 								<div class="btn-contain">
-									<a href="#" class="btn btn-orange">YES! UPGRADE MY ORDER <br> <span>For Only $69</span> </a>
+									<a href="#" class="btn btn-orange w-100">YES! UPGRADE MY ORDER <br> <span>For Only $69</span> </a>
 								</div>
 								<div class="icon-list">
 									<ul>
