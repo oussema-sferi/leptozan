27ee92b49960104e2c422a104051352263f87e04
oussema-sferi <oussema.sferi@gmail.com>
2025-01-25 19:05:19 +0100

    vsl - embedded video added

--- pages/vsl.php
+++ pages/vsl.php
@@ -59,14 +59,26 @@
 						<p>Finally, The Easiest Way To Achieve Your Dream Body</p>
 					</div> -->
 					<div class="vsl-video">
-						<video class="video" poster="images/video-poster.jpg">
-							<source src="images/mov_bbb.mp4" type="video/mp4">
+						<!--<video class="video" poster="images/video-poster.jpg">
+							<source src="/images/mov_bbb.mp4" type="video/mp4">
 						</video>
 						<a href="#" class="play-icon">
 							<figure>
-								<img src="images/play-icon.svg" alt="play-icon" class="img-fluid">
+								<img src="/images/play-icon.svg" alt="play-icon" class="img-fluid">
 							</figure>
-						</a>
+						</a>-->
+                        <div id="vidalytics_embed_QaH5Bsom_HORgX95" style="width: 100%; position:relative; padding-top: 56.25%;"></div>
+                        <script type="text/javascript">
+                            (function (v, i, d, a, l, y, t, c, s) {
+                                y='_'+d.toLowerCase();c=d+'L';if(!v[d]){v[d]={};}if(!v[c]){v[c]={};}if(!v[y]){v[y]={};}var vl='Loader',vli=v[y][vl],vsl=v[c][vl + 'Script'],vlf=v[c][vl + 'Loaded'],ve='Embed';
+                                if (!vsl){vsl=function(u,cb){
+                                    if(t){cb();return;}s=i.createElement("script");s.type="text/javascript";s.async=1;s.src=u;
+                                    if(s.readyState){s.onreadystatechange=function(){if(s.readyState==="loaded"||s.readyState=="complete"){s.onreadystatechange=null;vlf=1;cb();}};}else{s.onload=function(){vlf=1;cb();};}
+                                    i.getElementsByTagName("head")[0].appendChild(s);
+                                };}
+                                vsl(l+'loader.min.js',function(){if(!vli){var vlc=v[c][vl];vli=new vlc();}vli.loadScript(l+'player.min.js',function(){var vec=v[d][ve];t=new vec();t.run(a);});});
+                            })(window, document, 'Vidalytics', 'vidalytics_embed_QaH5Bsom_HORgX95', 'https://fast.vidalytics.com/embeds/oCMpPuWk/QaH5Bsom_HORgX95/');
+                        </script>
 					</div>
 				</div>
 			</div>
