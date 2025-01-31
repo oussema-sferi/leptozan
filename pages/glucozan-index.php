d5bbd2bc5dce986ee90077af4af271741bc47300
oussema-sferi <oussema.sferi@gmail.com>
2025-01-31 23:05:55 +0100

    glucozan - figma conversion - home page, header + footer done

---
+++ pages/glucozan-index.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <!-- header start -->
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/glucozan/components/header.php'; ?>
+    <!-- header end -->
+
+    <title>Glucozan Home</title>
+</head>
+<body>
+
+
+<!-- content start -->
+<div class="content">
+
+    <!-- HEADER SECTION Start -->
+    <section class="vsl-banner-wrp vsl-banner-wrp2 upsell-page" style="background-color: #D4F1F2;">
+        <div class="container">
+            <div class="header-text">
+                <p>Leptozan Ignites Your <br>Body’s <em>“Fat-Burning”</em> Mode <br>—Discover How to Achieve Even <em>FASTER</em> <br><em>Results</em> with Our New Breakthrough</p>
+            </div>
+            <div class="vsl-banner-main">
+                <div class="banner-order-steps">
+                    <div class="order-step">
+                        <div class="icon" style="background-color: #AB2D4C;">
+                            <figure>
+                                <img src="/images/Subtract.svg" alt="order-img" class="img-fluid">
+                            </figure>
+                        </div>
+                        <div class="order-details">
+                            <div class="small-title">
+                                <span>Order confirmation</span>
+                            </div>
+                            <div class="details" style="color: #979797;">
+                                <p>Leptozan</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="order-step active">
+                        <div class="icon" style="background-color: var(--white);">
+                            <div class="section-title" style="color: #AB2D4C;">
+                                <h2>2</span>
+                            </div>
+                        </div>
+                        <div class="order-details">
+                            <div class="small-title" style="color: var(--white);">
+                                <span>Special upgrade</span>
+                            </div>
+                            <div class="details" style="color: var(--white);">
+                                <p>Important Announcement</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="order-step">
+                        <div class="icon" style="background-color: #AB2D4C;">
+                            <div class="section-title">
+                                <h2>3</span>
+                            </div>
+                        </div>
+                        <div class="order-details">
+                            <div class="small-title">
+                                <span>Order Complete</span>
+                            </div>
+                            <div class="details" style="color: #979797;">
+                                <p>Shipping Initiated</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- HEADER SECTION End -->
+
+
+
+    <!-- special-deal-section -->
+    <!--<section class="upsell-special-deal-section special-deal-section" style="background: var(--grey3); display: none">
+        <div class="container">
+            <div class="special-deal-wrap">
+                <div class="special-deal-detail text-center">
+                    <div class="large-detail">
+                        <p>This is a Special Deal for</p>
+                    </div>
+                    <div class="section-title">
+                        <h2><span>New</span> Customers Only!</h2>
+                    </div>
+                </div>
+                <div class="special-deal-main">
+                    <div class="row">
+                        <div class="col-xl-6 col-lg-6 col-md-6">
+                            <div class="special-deal-left">
+                                <div class="special-deal-top-left text-center">
+                                    <div class="block-title">
+                                        <h3>Add an Additional <br> <span>3 Bottles To Your Order</span></h3>
+                                    </div>
+                                </div>
+                                <div class="special-deal-img text-center" style="background-color: var(--white);">
+                                    <figure>
+                                        <img width="370" src="/images/offers-img-3.png" alt="order-img" class="img-fluid">
+                                    </figure>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6">
+                            <div class="special-deal-right">
+                                <div class="saving-banner">
+                                    <div class="left-details text-center">
+                                        <div class="saving-details">
+                                            <span>Just</span>
+                                            <sup><b>$</b></sup>
+                                            <span>59</span>
+                                            <sup>Per
+                                                Bottle</sup>
+                                            <div class="details">
+                                                <p>YOU SAVE $120</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="right-details text-center" style="background: #41A3A3; color: var(--white);">
+                                        <div class="right-details-img">
+                                            <figure>
+                                                <img src="/images/total-saving.png" alt="total-saving" class="img-fluid">
+                                            </figure>
+                                        </div>
+                                        <div class="small-title">
+                                            <span>Total</span>
+                                        </div>
+                                        <div class="saving-details" style="color: var(--white);">
+                                            <sup><b style="color: var(--white);">$</b></sup>
+                                            <span>177</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="deal-list">
+                                    <ul>
+                                        <li><span>This is the absolute <strong> lowest price </strong>you’ll ever see for 3 bottles of  <strong>LEPTOZAN.</strong></span></li>
+                                        <li><span>It <strong> protects </strong>you from future price increases as well as <strong> out of stocks.</strong></span></li>
+                                    </ul>
+                                </div>
+                                <div class="btn-contain">
+                                    <a href="#" class="btn btn-orange w-100">YES! UPGRADE MY ORDER <br> <span>For Only $177</span> </a>
+                                </div>
+                                <div class="icon-list">
+                                    <ul>
+                                        <li><a href="#"><img src="/images/badge1.png" alt="badge"></a></li>
+                                        <li><a href="#"><img src="/images/badge2.png" alt="badge"></a></li>
+                                        <li><a href="#"><img src="/images/badge3.png" alt="badge"></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="details-bottom text-center">
+                    <p><a id="no-thanks-link" href="#">No, Thanks.</a> <a href="#" class="details-bottom-text">I understand this is the very last time I can get my hands on Leptozan. I also understand that I will never see this page or this discount again, and I am aware of the potential effects on my health of declining this offer.</a></p>
+                </div>
+            </div>
+        </div>
+    </section>-->
+    <!-- special-deal-section -->
+
+    <!-- NO-THANKS special-deal-section start-->
+
+    <!--<section class="no-thanks-special-deal-section special-deal-section" style="background: var(--grey3); display: none">
+        <div class="container">
+            <div class="special-deal-wrap">
+                <div class="special-deal-detail text-center">
+                    <div class="large-detail">
+                        <p>This is a Special Deal for</p>
+                    </div>
+                    <div class="section-title">
+                        <h2><span>New</span> Customers Only!</h2>
+                    </div>
+                </div>
+                <div class="special-deal-main">
+                    <div class="row">
+                        <div class="col-xl-6 col-lg-6 col-md-6">
+                            <div class="special-deal-left">
+                                <div class="special-deal-top-left text-center">
+                                    <div class="block-title">
+                                        <h3 class="title-blink">OK. Maybe 3 bottles was a little too much, but how about an <span>additional 2 BOTTLES?</span></h3>
+                                    </div>
+                                </div>
+                                <div class="special-deal-img text-center" style="background-color: var(--white);">
+                                    <figure>
+                                        <img width="370" src="/images/offers-img-1.png" alt="order-img" class="img-fluid">
+                                    </figure>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6">
+                            <div class="special-deal-right">
+                                <div class="saving-banner">
+                                    <div class="left-details text-center">
+                                        <div class="saving-details">
+                                            <span>Just</span>
+                                            <sup><b>$</b></sup>
+                                            <span class="price-blink">49</span>
+                                            <sup>Per
+                                                Bottle</sup>
+                                            <div class="details">
+                                                <p>YOU SAVE $120</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="right-details text-center" style="background: #41A3A3; color: var(--white);">
+                                        <div class="right-details-img">
+                                            <figure>
+                                                <img src="/images/total-saving.png" alt="total-saving" class="img-fluid">
+                                            </figure>
+                                        </div>
+                                        <div class="small-title">
+                                            <span>Total</span>
+                                        </div>
+                                        <div class="saving-details" style="color: var(--white);">
+                                            <sup><b style="color: var(--white);">$</b></sup>
+                                            <span>177</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="deal-list">
+                                    <ul>
+                                        <li><span>This is the absolute <strong> lowest price </strong>you’ll ever see for 2 bottles of  <strong>LEPTOZAN.</strong></span></li>
+                                        <li><span>It <strong> protects </strong>you from future price increases as well as <strong> out of stocks.</strong></span></li>
+                                    </ul>
+                                </div>
+                                <div class="btn-contain">
+                                    <a href="#" class="btn btn-orange w-100">YES! UPGRADE MY ORDER <br> <span>For Only $<span class="price">177</span></span> </a>
+                                </div>
+                                <div class="icon-list">
+                                    <ul>
+                                        <li><a href="#"><img src="/images/badge1.png" alt="badge"></a></li>
+                                        <li><a href="#"><img src="/images/badge2.png" alt="badge"></a></li>
+                                        <li><a href="#"><img src="/images/badge3.png" alt="badge"></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="details-bottom text-center">
+                    <p><a href="#">No, Thanks.</a> <a href="#">I understand this is the very last time I can get my hands on Leptozan. I also understand that I will never see this page or this discount again, and I am aware of the potential effects on my health of declining this offer. </a> </p>
+                </div>
+            </div>
+        </div>
+    </section>-->
+    <!-- NO-THANKS special-deal-section end-->
+
+    <!--<section class="official-page" style="background-color: #D4F1F2;display: none">
+        <div class="container">
+            <div class="official-page-wrap text-center">
+                <div class="official-page-top">
+                    <div class="block-title">
+                        <h3>Leptozan is uniquely available on this <br> <span>official page only.</span></h3>
+                    </div>
+                    <div class="detail">
+                        <p>(To ensure the best possible prices, it is NOT available on Amazon, eBay, Walmart, pharmacies, or anywhere else.)</p>
+                    </div>
+                </div>
+                <div class="official-page-logo">
+                    <ul>
+                        <li><a href="#"><img src="/images/amazon.png" alt="img"></a></li>
+                        <li><a href="#"><img src="/images/ebay.png" alt="img"></a></li>
+                        <li><a href="#"><img src="/images/walmart 1.png" alt="img"></a></li>
+                        <li><a href="#"><img src="/images/available.png" alt="img"></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>-->
+
+</div>
+<!-- content end -->
+
+<!-- footer start -->
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/glucozan/components/footer.php'; ?>
+<!-- footer end -->
+
+<!-- scripts start -->
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/components/scripts.php'; ?>
+<?php /*include $_SERVER['DOCUMENT_ROOT'] . '/js/custom/upsell.js'; */?>
+<script src="/js/custom/upsell.js"></script>
+<!-- scripts end -->
+
+</body>
+</html>
