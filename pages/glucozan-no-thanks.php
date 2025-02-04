1bdd226e0f7a9cb0006b928cb595af1d08ecbd87
oussema-sferi <oussema.sferi@gmail.com>
2025-02-04 05:08:26 +0100

    glucozan - no-thanks page - header banner + brushes almost fixed

---
+++ pages/glucozan-no-thanks.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <!-- header start -->
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/glucozan/components/header.php'; ?>
+    <!-- header end -->
+
+    <title>Glucozan Home</title>
+</head>
+<body>
+
+
+<!-- content start -->
+<div class="content">
+
+    <!-- Stop Section -->
+    <section class="stop-section">
+        <div class="container">
+            <div class="stop-content">
+                <!-- Background Brush -->
+                <img src="/images/glucozan/brush-2.png" alt="Brush Background" class="brush-background">
+
+                <!-- Text Content -->
+                <h2 class="stop-title">
+                <span class="brush-text">
+                    <img src="/images/glucozan/brush-1.png" alt="Brush Text Background">
+                    STOP!
+                </span>
+                </h2>
+                <p class="stop-subtitle">
+                    This is your <span class="highlight">last chance</span> to stock up <br>
+                    at our lowest price available...
+                </p>
+                <p class="stop-note">Only Available to New Customers!</p>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+</div>
+<!-- content end -->
+
+<!-- footer start -->
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/glucozan/components/footer.php'; ?>
+<!-- footer end -->
+
+<!-- scripts start -->
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/components/scripts.php'; ?>
+<?php /*include $_SERVER['DOCUMENT_ROOT'] . '/js/custom/upsell.js'; */?>
+<script src="/js/custom/upsell.js"></script>
+<!-- scripts end -->
+
+</body>
+</html>
