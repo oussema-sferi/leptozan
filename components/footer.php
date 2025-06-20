dcd83f878d0522e4b62729582c4fcbc72d8996e0
Oussema Sferi <oussema.sferi@gmail.com>
2025-06-20 01:12:01 +0100

    style(footer): change footer layout

--- components/footer.php
+++ components/footer.php
@@ -13,16 +13,20 @@
                 </ul>
             </div>
             <div class="footer-details">
-                <p>Statements on this website have not been evaluated by the Food and Drug Administration. Products are not intended to diagnose, treat, cure or prevent any disease</p>
+                <!--<p>Statements on this website have not been evaluated by the Food and Drug Administration. Products are not intended to diagnose, treat, cure or prevent any disease</p>
                 <p>The website's content and the product for sale is based upon the author's opinion and is provided solely on an "AS IS" and "AS AVAILABLE" basis. You should do your own research and confirm the information with other sources when searching for information regarding health issues and always review the information carefully with your professional health care provider before using any of the protocols presented on this website and/or in the product sold here. Neither BuyGoods nor the author are engaged in rendering medical or similar professional services or advice via this website or in the product, and the information provided is not intended to replace medical advice offered by a physician or other licensed healthcare provider. You should not construe BuyGoods's sale of this product as an endorsement by BuyGoods of the views expressed herein, or any warranty or guarantee of any strategy, recommendation, treatment, action, or application of advice made by the author of the product.</p>
-                <p>Some names and personal identifying information on this site have been changed to protect the privacy of individuals.</p>
+                <p>Some names and personal identifying information on this site have been changed to protect the privacy of individuals.</p>-->
+
                 <span id=disclaimer></span>
                 <script src="https://display.buygoods.com/v1/disclaimer?id=disclaimer&account_id=10880"></script>
+                <p class="mt-4">
+                    All statements and results presented on this website are for informational purposes only. They are not specific medical advice for any individual. Neither the website, nor product should substitute medical advice given by a certified health professional. If you have a health problem, or you have sensible allergies, are pregnant or diagnosed with chronic conditions, it is strongly recommended that you consult your doctor immediately and before taking any pills or supplements. Individual results may vary depending on the case. Copyright © 2025. All Rights Reserved.
+                </p>
             </div>
 
-            <div class="copyright">
-                <p>© <?php echo Date('Y')?>.Leptozan. All rights Reserved.</p>
-            </div>
+            <!--<div class="copyright">
+                <p>Copyright © <?php /*echo Date('Y')*/?>. All Rights Reserved.</p>
+            </div>-->
         </div>
     </div>
 </footer>
