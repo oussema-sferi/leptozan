9a96a0606295586995b81fa8b314c17d8d6c6870
Oussema Sferi <oussema.sferi@gmail.com>
2025-07-16 10:14:56 +0100

    fix(figmatocode): favicon added

--- pages/new-upsell.php
+++ pages/new-upsell.php
@@ -5,7 +5,7 @@
     <!--<meta name=description content="This site was generated with Anima. www.animaapp.com"/>-->
     <!-- <link rel="shortcut icon" type=image/png href="https://animaproject.s3.amazonaws.com/home/favicon.png" /> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <link rel="shortcut icon" type="image/png" href="https://animaproject.s3.amazonaws.com/home/favicon.png" />
+    <link rel="icon" type="image/png" href="/images/favicon-96x96.png" sizes="96x96" />
     <meta name="og:type" content="website" />
     <meta name="twitter:card" content="photo" />
     <link rel="stylesheet" type="text/css" href="/new-ups/css/index.css" />
